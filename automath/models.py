d54696e532d562125146d2ce0463883b3e85ec79
Mikael LE MENTEC <mikeleyeti@yahoo.fr>
2020-08-21 16:35:27 +0200

    Modification de Model pour compatibilité Django 2.x

--- automath/models.py
+++ automath/models.py
@@ -1,7 +1,4 @@
-from _ast import mod
-
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
@@ -10,30 +7,47 @@ class Question(models.Model):
     Modèle de question
     """
 
-    class Themes(models.TextChoices):
-        "Liste des thèmes accessibles"
-        ENTIERS = 'ENT', _('Entiers')
-        DECIMAUX = 'DEC', _('Démimaux')
-        RELATIFS = 'REL', _('Relatifs')
-        FRACTIONS = 'FRA', _('Fractions')
+    # class Themes(models.TextChoices):
+    #     "Liste des thèmes accessibles"
+    #     ENTIERS = 'ENT', _('Entiers')
+    #     DECIMAUX = 'DEC', _('Démimaux')
+    #     RELATIFS = 'REL', _('Relatifs')
+    #     FRACTIONS = 'FRA', _('Fractions')
+    class Theme(models.Model):
+        THEME_CHOICES = [
+            ('ENT', 'Entiers'),
+            ('DEC', 'Démimaux'),
+            ('REL', 'Relatifs'),
+            ('FRA', 'Fractions')
+        ]
 
-    class Niveaux(models.TextChoices):
-        "Liste des niveaux proposés"
-        SIXIEME = "6eme", _('Sixième')
-        CINQUIEME = "5eme", _('Cinquième')
-        QUATRIEME = "4eme", _('Quatrième')
-        TROISIEME = "3eme", _('Troisième')
-        SECONDE = "2nde", _('Seconde')
-        PREMIERE = "1ere", _('Première')
-        TERMINALE = "Term", _('Terminale')
+    #
+    # class Niveaux(models.TextChoices):
+    #     "Liste des niveaux proposés"
+    #     SIXIEME = "6eme", _('Sixième')
+    #     CINQUIEME = "5eme", _('Cinquième')
+    #     QUATRIEME = "4eme", _('Quatrième')
+    #     TROISIEME = "3eme", _('Troisième')
+    #     SECONDE = "2nde", _('Seconde')
+    #     PREMIERE = "1ere", _('Première')
+    #     TERMINALE = "Term", _('Terminale')
 
-
+    class Niveaux(models.Model):
+        THEME_NIVEAUX = [
+            ('6eme', 'Sixième'),
+            ('5eme', 'Cinquième'),
+            ('4eme', 'Quatrième'),
+            ('3eme', 'Troisième'),
+            ('2nde', 'Seconde'),
+            ('1ere', 'Première'),
+            ('Term', 'Terminale')
+        ]
 
     titre = models.CharField(max_length=200, primary_key=True)
     question = models.TextField(null=True)
-    theme = models.CharField(max_length=3, choices=Themes.choices)
+    theme = models.CharField(max_length=3, choices=Theme.THEME_CHOICES)
     enonce = models.TextField(null=False)
-    niveau = models.CharField(max_length=4,choices=Niveaux.choices)
+    niveau = models.CharField(max_length=4, choices=Niveaux.THEME_NIVEAUX)
 
     def __str__(self):
         return self.titre
